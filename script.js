f79332ef27b3aa9c0406cd120cc9d72493d5685c
bananas35 <140848506+bananas35@users.noreply.github.com>
2023-07-28 12:04:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function bananas_button() {
+  document.getElementById("bannas").scrollIntoView();
+}
+function oranges_button() {
+  document.getElementById("orange").scrollIntoView();
+}
+function apples_button() {
+  document.getElementById("apples").scrollIntoView();
+}
